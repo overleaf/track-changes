271a0428f07d1d44fb4b9213e2433baa740dd515
Alf Eaton <alf.eaton@overleaf.com>
2021-05-25 16:49:34 +0100

    Run format:fix

--- benchmark.js
+++ benchmark.js
@@ -13,83 +13,112 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-let i;
-const request = require("request");
-const rclient = require("redis").createClient();
-const async = require("async");
-const {ObjectId} = require("./app/js/mongojs");
+let i
+const request = require('request')
+const rclient = require('redis').createClient()
+const async = require('async')
+const { ObjectId } = require('./app/js/mongojs')
 
-const NO_OF_DOCS = 100;
-const NO_OF_UPDATES = 200;
+const NO_OF_DOCS = 100
+const NO_OF_UPDATES = 200
 
-const user_id = ObjectId().toString();
+const user_id = ObjectId().toString()
 
 const updates = (() => {
-	let asc, end;
-	const result = [];
-	for (i = 1, end = NO_OF_UPDATES, asc = end >= 1; asc ? i <= end : i >= end; asc ? i++ : i--) {
-		result.push({
-			op: { i: "a", p: 0 },
-			v: i,
-			meta: { ts: new Date(), user_id
-		}
-		});
-	}
-	return result;
-})();
-const jsonUpdates = (Array.from(updates).map((u) => JSON.stringify(u)));
+  let asc, end
+  const result = []
+  for (
+    i = 1, end = NO_OF_UPDATES, asc = end >= 1;
+    asc ? i <= end : i >= end;
+    asc ? i++ : i--
+  ) {
+    result.push({
+      op: { i: 'a', p: 0 },
+      v: i,
+      meta: { ts: new Date(), user_id }
+    })
+  }
+  return result
+})()
+const jsonUpdates = Array.from(updates).map((u) => JSON.stringify(u))
 
-const doc_ids = ((() => {
-	let asc1, end1;
-	const result1 = [];
-	for (i = 1, end1 = NO_OF_DOCS, asc1 = end1 >= 1; asc1 ? i <= end1 : i >= end1; asc1 ? i++ : i--) {
-		result1.push(ObjectId().toString());
-	}
-	return result1;
-})());
+const doc_ids = (() => {
+  let asc1, end1
+  const result1 = []
+  for (
+    i = 1, end1 = NO_OF_DOCS, asc1 = end1 >= 1;
+    asc1 ? i <= end1 : i >= end1;
+    asc1 ? i++ : i--
+  ) {
+    result1.push(ObjectId().toString())
+  }
+  return result1
+})()
 
-const populateRedis = function(callback) {
-	if (callback == null) { callback = function(error) {}; }
-	console.log("Populating Redis queues...");
+const populateRedis = function (callback) {
+  if (callback == null) {
+    callback = function (error) {}
+  }
+  console.log('Populating Redis queues...')
 
-	const jobs = [];
-	for (const doc_id of Array.from(doc_ids)) {
-		((doc_id => jobs.push(callback => rclient.rpush(`UncompressedHistoryOps:${doc_id}`, ...Array.from(jsonUpdates), callback))))(doc_id);
-	}
-	return async.series(jobs, function(error) {
-		if (error != null) { return callback(error); }
-		console.log("Done.");
-		return callback();
-	});
-};
+  const jobs = []
+  for (const doc_id of Array.from(doc_ids)) {
+    ;((doc_id) =>
+      jobs.push((callback) =>
+        rclient.rpush(
+          `UncompressedHistoryOps:${doc_id}`,
+          ...Array.from(jsonUpdates),
+          callback
+        )
+      ))(doc_id)
+  }
+  return async.series(jobs, function (error) {
+    if (error != null) {
+      return callback(error)
+    }
+    console.log('Done.')
+    return callback()
+  })
+}
 
-const flushDocs = function(callback) {
-	if (callback == null) { callback = function(error) {}; }
-	console.log("Flushing docs...");
-	let inProgress = 0;
-	const jobs = [];
-	for (const doc_id of Array.from(doc_ids)) {
-		((doc_id => jobs.push(function(callback) {
-            inProgress = inProgress + 1;
-            return request.post(`http://localhost:3014/doc/${doc_id}/flush`, function(error) {
-                inProgress = inProgress - 1;
-                console.log(Date.now(), `In progress: ${inProgress}`);
-                return callback(error);
-            });
-        })))(doc_id);
-	}
-	return async.parallel(jobs, function(error) {
-		if (error != null) { return callback(error); }
-		console.log("Done.");
-		return callback();
-	});
-};
+const flushDocs = function (callback) {
+  if (callback == null) {
+    callback = function (error) {}
+  }
+  console.log('Flushing docs...')
+  let inProgress = 0
+  const jobs = []
+  for (const doc_id of Array.from(doc_ids)) {
+    ;((doc_id) =>
+      jobs.push(function (callback) {
+        inProgress = inProgress + 1
+        return request.post(
+          `http://localhost:3014/doc/${doc_id}/flush`,
+          function (error) {
+            inProgress = inProgress - 1
+            console.log(Date.now(), `In progress: ${inProgress}`)
+            return callback(error)
+          }
+        )
+      }))(doc_id)
+  }
+  return async.parallel(jobs, function (error) {
+    if (error != null) {
+      return callback(error)
+    }
+    console.log('Done.')
+    return callback()
+  })
+}
 
-populateRedis(function(error) {
-	if (error != null) { throw error; }
-	return flushDocs(function(error) {
-		if (error != null) { throw error; }
-		return process.exit(0);
-	});
-});
-
+populateRedis(function (error) {
+  if (error != null) {
+    throw error
+  }
+  return flushDocs(function (error) {
+    if (error != null) {
+      throw error
+    }
+    return process.exit(0)
+  })
+})
--- fixdangling.js
+++ fixdangling.js
@@ -10,67 +10,82 @@
  * DS207: Consider shorter variations of null checks
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const Settings = require("settings-sharelatex");
-const logger = require("logger-sharelatex");
-const TrackChangesLogger = logger.initialize("track-changes").logger;
-const async = require("async");
-const fs = require("fs");
-const request = require("request");
-const cli = require("cli");
+const Settings = require('settings-sharelatex')
+const logger = require('logger-sharelatex')
+const TrackChangesLogger = logger.initialize('track-changes').logger
+const async = require('async')
+const fs = require('fs')
+const request = require('request')
+const cli = require('cli')
 
-const mongojs = require("mongojs");
-const bson = require("bson");
-const db = mongojs(Settings.mongo.url, ["docs"]);
-const {
-    ObjectId
-} = mongojs;
+const mongojs = require('mongojs')
+const bson = require('bson')
+const db = mongojs(Settings.mongo.url, ['docs'])
+const { ObjectId } = mongojs
 
 const options = cli.parse({
-	port: ['p', 'port number for track changes', 'number'],
-	force: ['f', 'actually make the fix']
-});
+  port: ['p', 'port number for track changes', 'number'],
+  force: ['f', 'actually make the fix']
+})
 
 if (cli.args.length < 1) {
-	console.log("fixdangling -p PORT file_of_doc_ids");
-	process.exit();
+  console.log('fixdangling -p PORT file_of_doc_ids')
+  process.exit()
 }
 
-const file = cli.args.pop();
-const doc_ids = fs.readFileSync(file).toString().trim().split("\n");
+const file = cli.args.pop()
+const doc_ids = fs.readFileSync(file).toString().trim().split('\n')
 
-let missing = 0;
-let errored = 0;
-let success = 0;
+let missing = 0
+let errored = 0
+let success = 0
 
-const fixDangling = (doc_id, callback) => // look up project id from doc id
-db.docs.find({_id:ObjectId(doc_id)}, {project_id:1}, function(err, result) {
+const fixDangling = (
+  doc_id,
+  callback // look up project id from doc id
+) =>
+  db.docs.find({ _id: ObjectId(doc_id) }, { project_id: 1 }, function (
+    err,
+    result
+  ) {
     // console.log "doc_id", doc_id, "err", err, "result", result
     if (err != null) {
-        errored++;
-        return callback();
+      errored++
+      return callback()
     }
-    if ((result == null) || (result.length === 0)) {
-        missing++;
-        return callback();
+    if (result == null || result.length === 0) {
+      missing++
+      return callback()
     }
-    const {
-        project_id
-    } = result[0];
-    console.log("found project_id", project_id, "for doc_id", doc_id);
-    const url = `http://localhost:${options.port}/project/${project_id}/doc/${doc_id}/flush`;
+    const { project_id } = result[0]
+    console.log('found project_id', project_id, 'for doc_id', doc_id)
+    const url = `http://localhost:${options.port}/project/${project_id}/doc/${doc_id}/flush`
     if (options.force) {
-        return request.post(url, function(err, response, body) {
-            if (err != null) { errored++; } else { success++; }
-            return callback();
-        });
+      return request.post(url, function (err, response, body) {
+        if (err != null) {
+          errored++
+        } else {
+          success++
+        }
+        return callback()
+      })
     } else {
-        console.log("URL:", url);
-        success++;
-        return callback();
+      console.log('URL:', url)
+      success++
+      return callback()
     }
-});
+  })
 
-async.eachSeries(doc_ids, fixDangling, function(err) {
-	console.log("final result", err, "missing", missing, "errored", errored, "success", success);
-	return db.close();
-});
+async.eachSeries(doc_ids, fixDangling, function (err) {
+  console.log(
+    'final result',
+    err,
+    'missing',
+    missing,
+    'errored',
+    errored,
+    'success',
+    success
+  )
+  return db.close()
+})
